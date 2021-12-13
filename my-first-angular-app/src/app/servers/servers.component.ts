71beef5fdf29d4a7ef8ecc8707a95a42d7f1466f
Wei-bin-Wu <weibin789123@gmail.com>
2021-12-13 13:48:34 +0100

    [fix] Ep 2.34 - Data binding

--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000); // after 2 second allwNewServer = true
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any){
+    // console.log(event);
+    this.serverName = event.target.value; // (<HTMLInputElement>event.target).value --> casting
+  }
 }
