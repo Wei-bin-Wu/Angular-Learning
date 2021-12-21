47f8100e65c29b0b1a03fbd964ea39b676fbb729
Wei-bin-Wu <weibin789123@gmail.com>
2021-12-21 10:32:15 +0100

    [fix] Ep 10.122 - Add ingredients to recipes

--- course-app-recipe-shopping/src/app/recipes/recipe.service.ts
+++ course-app-recipe-shopping/src/app/recipes/recipe.service.ts
@@ -1,14 +1,23 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('TestRecipe1','test description',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('French Fries', 20)
+      ]),
     new Recipe('TestRecipe2','test description',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new Ingredient('Buns',1),
+        new Ingredient('Meat',1)
+      ])
   ];
 
   getRecipes() {
