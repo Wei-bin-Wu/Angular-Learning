d9982893f1cd71c0088b2db273b4a8a862a6894d
Wei-bin-Wu <weibin789123@gmail.com>
2021-12-30 17:29:28 +0100

    [fix] Ep 19.283 - Fetch data

--- course-app-recipe-shopping/src/app/shared/data-storage.service.ts
+++ course-app-recipe-shopping/src/app/shared/data-storage.service.ts
@@ -18,9 +18,18 @@ export class DataStorageService {
     this.http
       .put(
         'https://angularcourse-1c491-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-        recipes
-      ).subscribe(response => {
+        recipes)
+      .subscribe(response => {
         console.log(response);
+      });
+  }
+
+  fetchRecipes(){
+    this.http
+      .get<Recipe[]>(
+      'https://angularcourse-1c491-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
       });
   }
 }
