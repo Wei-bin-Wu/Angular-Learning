f8299bf713ca75957c873a6de6a65570db024a8b
Wei-bin-Wu <weibin789123@gmail.com>
2021-12-30 17:21:32 +0100

    [fix] Ep 19.282 - Store data

--- course-app-recipe-shopping/src/app/shared/data-storage.service.ts
+++ course-app-recipe-shopping/src/app/shared/data-storage.service.ts
@@ -1,9 +1,26 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Recipe} from "../recipes/recipe.model";
+import {RecipeService} from "../recipes/recipe.service";
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http:HttpClient) {
+  constructor(private http:HttpClient,
+              private recipeService: RecipeService) {}
+
+  // one way
+  // storeRecipes(recipes: Recipe[]){}
+
+  // another way recipe service
+  storeRecipes(){
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://angularcourse-1c491-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      ).subscribe(response => {
+        console.log(response);
+      });
   }
 }
